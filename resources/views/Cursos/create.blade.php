4ae3eca7e0ba1be348f32f659dc1e681be2f900d
robydinca <robertmdinca@proton.me>
2024-03-02 12:36:58 +0100

    first commit

---
+++ resources/views/Cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.plantilla')
+
+@section('title', 'Cursos')
+
+@section('content')
+  <h1>pagina de creacion de cursos</h1>
+@endsection
---
+++ resources/views/Cursos/index.blade.php
@@ -0,0 +1,8 @@
+@extends ('layouts.plantilla')
+
+@section('title', 'Cursos')
+
+@section('content')
+  <h1>pagina de cursos</h1>
+@endsection
+
